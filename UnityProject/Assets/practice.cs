3ab1f406aa2f500222cda69c67d5a50c66dbb0a6
a0930014306 <67904630+a0930014306@users.noreply.github.com>
2021-01-02 20:51:02 +0800

    欄位練習

---
+++ UnityProject/Assets/practice.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class practice : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float Speed = 50;
+    [Header("跳躍高度"), Range(200, 1000)]
+    public int Jump = 700;
+    [Header("對話內容")]
+    public string Talk;
+    [Header("是否取得雞蛋")]
+    public bool Egg = false;
+
+
+    public void Start()
+    {
+        print("遊戲開始!");
+    }
+
+    private void Update()
+    {
+        print(Talk);
+    }
+}
